f2ef5fe0dad2a106cd402660de53f162b07f1a21
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-24 12:22:52 -0400

    Create 5.1.3 Zombies Part 2.py

---
+++ 5.1.3 Zombies Part 2.py
@@ -0,0 +1,57 @@
+import random
+
+app.background = 'gold'
+
+infectedZone = Rect(0, 0, 1, 400, fill='crimson')
+people = Group()
+
+def drawPerson(cx, cy):
+    head = Circle(cx, cy, 20, fill='tan')
+    person = Group(
+        head,
+
+        # face
+        Circle(cx + 8, cy - 5, 2),
+        Circle(cx - 8, cy - 5, 2),
+        Circle(cx, cy + 5, 5)
+        )
+    person.head = head
+
+
+    # Randomly assign if the person is immune or not, and if they are draw
+    # a blue "halo" behind them.
+    isImmuneOptions = [ True, False ]
+    person.isImmune = choice(isImmuneOptions)
+    if (person.isImmune==True):
+        halo = Circle(person.centerX,person.centerY,25,fill="skyBlue")
+        people.add(halo)
+
+
+    people.add(person)
+
+def createPeople():
+
+    # Create 15 people randomly positioned in the canvas.
+    for a in range(15):
+        drawPerson(random.randint(0,400),random.randint(0,400))
+
+
+def moveInfectedZone(x):
+    #### START OF BLOCK MOVE_INFECTED ####
+
+    # If we clicked to the right of the infected zone's right edge, increase the
+    # width of the infected zone to be 1 pixel more than the x position.
+    ### Place Your Code Here ###
+    if (not infectedZone.hits(x,200)==True):
+        infectedZone.width=x+1
+
+    #### END OF BLOCK ####
+
+def onMousePress(mouseX, mouseY):
+    #### START OF BLOCK MOUSE_PRESS ####
+
+    # Call the moveInfectedZone function with the mouse's position as an argument.
+    ### Place Your Code Here ###
+    moveInfectedZone(mouseX)
+
+    #### END OF BLOCK ####
